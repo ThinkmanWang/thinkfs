038b07db314b7eae03acc8e36008a82d14f3dfdd
Thinkman Wang <wangxf1985@gmail.com>
2022-02-16 14:02:00 +0800

    update

--- thinkfs.c
+++ thinkfs.c
@@ -2,37 +2,37 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
-#define THINKFS_ROOT "thinkfs"
-static struct proc_dir_entry *thinkfs_root = NULL;
+//#define THINKFS_ROOT "thinkfs"
+//static struct proc_dir_entry *thinkfs_root = NULL;
 
 static int hello_proc_show(struct seq_file *m, void *v) {
-  seq_printf(m, "Hello World!\n");
-  return 0;
+    seq_printf(m, "Hello World!\n");
+    return 0;
 }
 
 static int hello_proc_open(struct inode *inode, struct  file *file) {
-  return single_open(file, hello_proc_show, NULL);
+    return single_open(file, hello_proc_show, NULL);
 }
 
 static const struct proc_ops hello_proc_fops = {
-  .proc_open = hello_proc_open,
-  .proc_read = seq_read,
-  .proc_lseek = seq_lseek,
-  .proc_release = single_release,
+    .proc_open = hello_proc_open,
+    .proc_read = seq_read,
+    .proc_lseek = seq_lseek,
+    .proc_release = single_release,
 };
 
 static int __init hello_proc_init(void) {
-    thinkfs_root = proc_mkdir(THINKFS_ROOT, NULL);
-    if (NULL == thinkfs_root) {
-        printk("cannot mkdir thinkfs");
-        return -ENOMEM;
-    }
-  proc_create("hellofs", 0, thinkfs_root, &hello_proc_fops);
-  return 0;
+    //thinkfs_root = proc_mkdir(THINKFS_ROOT, NULL);
+    //if (NULL == thinkfs_root) {
+    //    printk("cannot mkdir thinkfs");
+    //    return -ENOMEM;
+    //}
+    proc_create("hellofs", 0, NULL, &hello_proc_fops);
+    return 0;
 }
 
 static void __exit hello_proc_exit(void) {
-  remove_proc_entry("hellofs", NULL);
+    remove_proc_entry("hellofs", NULL);
 }
 
 MODULE_LICENSE("GPL");
